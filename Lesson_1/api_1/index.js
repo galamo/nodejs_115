0b6c7fe5b5c230f99a31b97da120b32acf305a12
AssafFink <assaf.fink@gmail.com>
2025-07-20 20:42:54 +0300

    a

--- Lesson_1/api_1/index.js
+++ Lesson_1/api_1/index.js
@@ -3,13 +3,51 @@ const PORT = 3000
 const app = express();
 
 
+
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}]=>${req.originalUrl}`)
+    next()
+})
+
 app.get("/hc", (req, res, next) => {
-    const queryParams = req.query;
-    console.log(queryParams)
-    res.send({ message: `Api is up _ ${new Date().toISOString()}` })
+    res.json({ message: `Api is up _ ${new Date().toISOString()}` })
+})
+
+app.use((req, res, next) => {
+    const key = req.query.key
+    if (key === "api_token") {
+        return next()
+    } else {
+        return next(new Error("UNAUTH"))
+    }
+
+})
+
+app.get("/users", (req, res, next) => {
+    res.json({ message: "users" })
 })
 
 
+
+app.use((error, req, res, next) => {
+    switch (error.message) {
+        case "UNAUTH": {
+            return res.status(401).send("Unauthorized___")
+        }
+        default: {
+            return res.status(500).send("Something went wrong Yam is working to fix it & flight to America")
+        }
+    }
+
+})
+
+// GET // /users/:id
+// DELETE /users/:id
+// POST /login {user:1,email:2,password:l}
+// PUT /users:id {user:1,email:2,password:l}
+// OPTIONS
+// HEAD 
 
 app.listen(PORT, (err) => {
     if (err) {
