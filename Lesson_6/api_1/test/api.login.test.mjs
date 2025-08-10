533e47508f8254249b2d361f7d14196084dac1d1
AssafFink <assaf.fink@gmail.com>
2025-08-10 19:15:07 +0300

    proj_1_db

---
+++ Lesson_6/api_1/test/api.login.test.mjs
@@ -0,0 +1,20 @@
+// const chai = require("chai")
+import { expect } from "chai";
+import axios from "axios";
+// const { expect } = chai;
+// const axios = require("axios");
+
+const URL = "http://localhost:3000/auth/";
+
+describe("Test Login API POST /Login", () => {
+  it("login - bad request, user name is missing", async () => {
+    try {
+      const result = await axios.post(URL + "login", {
+        password: "1234ww",
+      });
+      throw new Error({ status: 500 });
+    } catch (error) {
+      expect(error.status).equal(400);
+    }
+  });
+});
