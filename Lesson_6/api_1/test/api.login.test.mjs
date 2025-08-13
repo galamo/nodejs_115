5d206d1f3054f579f4ede218906d9a906ab9ca7f
AssafFink <assaf.fink@gmail.com>
2025-08-13 18:40:45 +0300

    a

--- Lesson_6/api_1/test/api.login.test.mjs
+++ Lesson_6/api_1/test/api.login.test.mjs
@@ -1,10 +1,32 @@
 // const chai = require("chai")
 import { expect } from "chai";
 import axios from "axios";
+import mysql2 from "mysql2/promise"
+import dotenv from "dotenv"
+dotenv.config()
 // const { expect } = chai;
 // const axios = require("axios");
 
 const URL = "http://localhost:3000/auth/";
+let connection;
+before(async function () {
+
+    try {
+        connection = await mysql2.createConnection({
+            host: process.env.HOST,
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+            port: Number(process.env.DB_PORT) || 3306
+        })
+        return connection
+
+    } catch (error) {
+        throw error;
+    }
+
+
+})
 
 describe("Test Login API POST /Login", () => {
     it("login - bad request, user name is missing", async () => {
@@ -29,7 +51,7 @@ describe("Test Login API POST /Login", () => {
         }
     });
     it("login - success authenticating user", async () => {
-        // not finished yet!
+        console.log(result2)
         const result = await axios.post(URL + "login", {
             password: "1112212",
             userName: "yam@gmail.com",
