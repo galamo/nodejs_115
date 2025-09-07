d3ad98994f08488e07d0b49908ae1eb23b8e6f0b
AssafFink <assaf.fink@gmail.com>
2025-09-07 19:24:48 +0300

    a

--- Lesson_8/Docker_api_env/index.js
+++ Lesson_8/Docker_api_env/index.js
@@ -3,14 +3,21 @@
 const express = require('express');
 const app = express();
 require("dotenv").config()
-const PORT = process.env.PORT || 3500;
+const { PORT, MESSAGE_HC, SECRET } = process.env
 
+function checkEnvParams() {
+    if (!PORT || !MESSAGE_HC || !SECRET) {
+        console.log('\x1b[31m%s\x1b[0m', 'MISSING ENV PARAMS');
+    }
+}
+checkEnvParams()
 // Health check endpoint
 app.get('/hc', (req, res) => {
-    res.send(`${process.env.MESSAGE_HC + new Date().toISOString()}`);
+    res.send(`${MESSAGE_HC + new Date().toISOString()}`);
 });
 
+console.log(SECRET)
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT || 3500, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- Lesson_8/readme.md
+++ Lesson_8/readme.md
@@ -1,7 +1,7 @@
 # Dockerizing Nodejs app
 
-1. docker build -t api .
-2. docker run -p 3500:3500 --env-file .env api
+1. docker build -t api-env-1 .
+2. docker run -p 3500:3500 --env-file .env api-env-1
 
 # Ex 
 1. Create a new Environment variables:
