df57a8ef80d0e4657c9da0796bafc302e035e756
Gal Amouyal <galamouy@Gals-MBP-2.lan>
2025-11-16 20:56:51 +0200

    mfs-1

--- mfs_code/MFS/Lab_5/mfs-clients/container/src/App.tsx
+++ mfs_code/MFS/Lab_5/mfs-clients/container/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 // @ts-ignore
 import CartPage from "Cart/CartPage";
@@ -6,27 +6,29 @@ import CartPage from "Cart/CartPage";
 // @ts-ignore
 import ProductsPage from "Products/ProductsPage";
 // @ts-ignore
-import SingleProduct from "Products/SingleProduct";
 import "./index.css";
 
-const App = () => (
-  <div className="container">
-    <h1 style={{ fontSize: "3rem" }}> Container -   Host  </h1>
-    <div className="host">
-      <div className="mfItem" style={{ overflow: "auto", maxHeight: "600px" }}>
-        <h1>Products</h1>
-        <div className="content">
-          <ProductsPage />
-          <SingleProduct />
+const App = () => {
+  return (
+    <div className="container">
+      <h1 style={{ fontSize: "3rem" }}> Container - Host </h1>
+      <div className="host">
+        <div
+          className="mfItem"
+          style={{ overflow: "auto", maxHeight: "600px" }}
+        >
+          <h1>Products</h1>
+          <div className="content">{<ProductsPage />}</div>
         </div>
-      </div>
-      <div className="mfItem">
-        <h1> Your Cart </h1>
-        <div className="content">
-          <CartPage />
+        <div className="mfItem">
+          <h1> Your Cart </h1>
+          <div className="content">
+            <CartPage />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
 ReactDOM.render(<App />, document.getElementById("app"));
